ef7c450ee69cd92c2247eda452d4a0dad43e073a
robin828 <61390860+robin828@users.noreply.github.com>
2021-09-02 20:15:31 +0530

    working

--- readPid.c
+++ readPid.c
@@ -71,7 +71,7 @@ int main(void) {
     }
 
     for(int i=0;i<10;i++) {
-        printf("%5d %-20s: %lu\n", ans[i].pid, ans[i].processName, ans[i].uTime);
+        printf("%5d : %lu\n", ans[i].pid, ans[i].uTime);
     }
 
     closedir(procdir);
